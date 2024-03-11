214d13b44d1b457a1a5f9108463c93942f949ecc
phounexxx <denissotovggg223@gmail.com>
2024-03-11 18:35:57 +0300

    initial spring boot skeleton

---
+++ the-assembly-company/src/main/java/dayatoxic/theassemblycompany/TheAssemblyCompanyApplication.java
@@ -0,0 +1,13 @@
+package dayatoxic.theassemblycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheAssemblyCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheAssemblyCompanyApplication.class, args);
+	}
+
+}
